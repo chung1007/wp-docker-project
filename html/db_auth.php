3530571d85c602099f38b67b4d2a6534ce74a0a6
Sam Chung <chung.woonyong@gmail.com>
2021-04-01 21:59:11 -0400

    fix db_auth mysql query bug

--- html/db_auth.php
+++ html/db_auth.php
@@ -7,16 +7,7 @@
 	if ($db->connect_error) {
 		die('Cannot connect to the database: ' . $db->connect_error);
 	}
-	#$db = mysqli_init();
-	#if (!$db) {
-    #	die('mysqli_init failed');
-	#}
-	
-	#if (!$db->real_connect($myHost, $myUserName, $myPassword, $myDBName)) {
-	#	die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
-	#}
-
-	echo 'Success! ' . $db->host_info . "\n";
+	echo "<script>console.log('DB Connection successful.');</script>";
 
 	function getDB()
 	{
@@ -29,29 +20,21 @@
 		global $db;
 		echo "<script>console.log('LOGIN: $login');</script>";
 		echo "<script>console.log('PASSWORD: $password');</script>";
-		#$result = $db->query("SELECT * FROM users WHERE password = '" . $password . "' and user = '" . $login . "'");
-		#$db_query = $db->query("SELECT DATABASE()");
-		#$row = $db_query->fetch_row();
-		#$default = $row[0];
-		#echo "<script>console.log('Default DB: $default');</script>";
-		$result = mysqli_query("SELECT * FROM wp_users WHERE user_pass = MD5('$password') AND user_login = '$login'");
-		echo "<script>console.log('HEREEEEE');</script>";
-		#$result = $db->query($sql);
-		echo "<script>console.log('RESULT: $result');</script>";
-		if (!$result) {
-			echo "<script>console.log('Login Unsuccessful.');</script>";
-			$message  = "\nInvalid Login\n";
-			echo "<script>console.log('$message');</script>";
-		}
-		else {
-			echo "<script>console.log('Login Successful.');</script>";
-			$row = array();
-			$row = mysqli_fetch_array($result);
-			if (!empty($row)) {
-				return $row;
+		
+		$db_query = $db->query("SELECT DATABASE()");
+		$row = $db_query->fetch_row();
+		$default = $row[0];
+		echo "<script>console.log('Default DB: $default');</script>";
+		
+		$result = $db->query("SELECT * FROM wp_users WHERE user_login = '$login' AND user_pass = MD5('$password')");
+		if ($result){
+			echo "<script>console.log('got result.');</script>";
+			$row = $result->fetch_array(MYSQLI_NUM);
+			if($row[1] && $row[2]){
+				echo "<script>console.log('Login success');</script>";
+				return true
 			}
 		}
-		mysqli_free_result($result);
 		return false;
 	}
 ?>
