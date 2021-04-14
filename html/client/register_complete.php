d60e8c8db2b3b3c66f044a921b726e8c5333201a
Sam Chung <chung.woonyong@gmail.com>
2021-04-14 00:36:30 -0400

    redirect to login after successful registrregistration.

--- html/client/register_complete.php
+++ html/client/register_complete.php
@@ -3,6 +3,10 @@
 		<title> Success </title>
 	</head>
 	<body>
-		<center><h2> Registration Complete. Thank you. </h2></center>
+		<center>
+			<h2> Registration Complete. Thank you! </h2>
+			<br> <p> You will now be redirected to login </p>
+		</center>
 	</body>
+	<meta http-equiv="refresh" content="5; URL=client.php" />
 </html>
