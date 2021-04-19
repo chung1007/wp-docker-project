0ddefa507a2c652f19cc2be325dbc4cf2d951db9
Sam Chung <chung.woonyong@gmail.com>
2021-04-19 19:50:51 -0400

    add automcomplete rule to fields in client side

--- html/client/home.php
+++ html/client/home.php
@@ -8,15 +8,15 @@
 	</head>
 	<body>
 		<h2> Client Portal </h2><br>
-		<form method="post" action="client_appointment.php">
+		<form method="post" action="client_appointment.php" autocomplete="off">
 			<h3> Vaccine Appoinment Registration </h3>
 			<hr><br>
-			<p>First name:  <input type="text" name="fname" size="15" maxlength="15" required></p>
-			<p>Last name:  <input type="password" name="lname" size="15" maxlength="15" required></p><br>
+			<p>First name:  <input type="text" name="fname" size="15" maxlength="15" required autocomplete="off"></p>
+			<p>Last name:  <input type="password" name="lname" size="15" maxlength="15" required autocomplete="off"></p><br>
 
-			<p> SSN:  <input type="text" name="ssn" size="15" maxlength="15" required></p>
-			<p> Healthcare Provider:  <input type="text" name="hc_provider" size="25" maxlength="25" required></p>
-			<p> Healthcare ID:  <input type="text" name="hc_id" size="25" maxlength="25" required></p>
+			<p> SSN:  <input type="text" name="ssn" size="15" maxlength="15" required autocomplete="off"></p>
+			<p> Healthcare Provider:  <input type="text" name="hc_provider" size="25" maxlength="25" required autocomplete="off"></p>
+			<p> Healthcare ID:  <input type="text" name="hc_id" size="25" maxlength="25" required autocomplete="off"></p>
 			<br><p><input type="submit" value="Register"></p>
 		</form>
 	</body>
