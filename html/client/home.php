41d10c39b178dd4d67409909dc3c76910d0f19c2
Sam Chung <chung.woonyong@gmail.com>
2021-05-14 19:06:34 -0400

    decorate vacc page

--- html/client/home.php
+++ html/client/home.php
@@ -16,20 +16,20 @@
 <html>
 	<head>
 		<title>Home</title>
-		<link rel="stylesheet" type="text/css" href="../css/template.css"/>
+		<link rel="stylesheet" type="text/css" href="client_template.css"/>
 		<!-- <link rel="stylesheet" type="text/css" href="css/global.6.css" /> -->
 	</head>
-	<body>
-		<h2> Client Portal </h2><br>
+	<body class="client-home">
+		<div class="header-div">
+		<h2 class="vacc-header"> Vaccine Appointment Registration </h2><br>
+		</div><br>
 		<form method="post" action="client_appointment.php" autocomplete="off">
-			<h3> Vaccine Appoinment Registration </h3>
-			<hr><br>
-			<p>First name:  <input type="text" name="fname" size="15" maxlength="15" placeholder="First" required autocomplete="off"></p>
-			<p>Last name:  <input type="password" name="lname" size="15" maxlength="15" placeholder="Last" required autocomplete="off"></p><br>
+			<p><strong>First name:&emsp;</strong><input type="text" name="fname" size="15" maxlength="15" placeholder="First" required autocomplete="off"></p>
+			<p><strong>Last name:&emsp;</strong><input type="password" name="lname" size="15" maxlength="15" placeholder="Last" required autocomplete="off"></p><br>
 
-			<p> SSN:  <input type="text" name="ssn" size="15" maxlength="15" placeholder="Your SSN" required autocomplete="off"></p>
-			<p> Healthcare Provider:  <input type="text" name="hc_provider" size="25" maxlength="25" placeholder="Your healthcare provider" required autocomplete="off"></p>
-			<p> Healthcare ID:  <input type="text" name="hc_id" size="25" maxlength="25" placeholder="Your healthcare ID" required autocomplete="off"></p>
+			<p><strong>SSN:&emsp;</strong><input type="text" name="ssn" size="15" maxlength="15" placeholder="Your SSN" required autocomplete="off"></p>
+			<p><strong>Healthcare Provider:&emsp;</strong><input type="text" name="hc_provider" size="25" maxlength="25" placeholder="Your healthcare provider" required autocomplete="off"></p>
+			<p><strong>Healthcare ID:&emsp;</strong><input type="text" name="hc_id" size="25" maxlength="25" placeholder="Your healthcare ID" required autocomplete="off"></p>
 			<br><p><input type="submit" value="Register"></p>
 		</form>
 	</body>
